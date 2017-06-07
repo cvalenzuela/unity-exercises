f621313fe575d5b9c89e74797d56018d65eaeb1a
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2017-06-07 17:14:13 -0400

    first commit

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb; // variable to store the reference of the rigidbody object.
+
+	// Start runs only at the beggining. In the first frame.
+	void Start () {
+		rb = GetComponent<Rigidbody> (); // attach the object's Rigidbody property to the variable rd
+	}
+
+
+	// FixedUpdate runs every frame. Just like update but when physics are involved
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+}
