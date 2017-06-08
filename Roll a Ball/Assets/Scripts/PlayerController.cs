caa941e553a093d2a2e47a4d0b23bcf89adb72dc
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2017-06-07 21:40:58 -0400

    roll a ball complete

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	private Rigidbody rb; // variable to store the reference of the rigidbody object.
+	private int count;
 
 	// Start runs only at the beggining. In the first frame.
 	void Start () {
 		rb = GetComponent<Rigidbody> (); // attach the object's Rigidbody property to the variable rd
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 
 
@@ -24,4 +31,20 @@ public class PlayerController : MonoBehaviour {
 
 		rb.AddForce (movement * speed);
 	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Pick Up")){
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText () {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You Win!";
+		}
+	}
+		
 }
